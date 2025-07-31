2023236e81050e306f6cd1332b66177179d14792
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-07-31 22:58:11 +0700

    Time: 0 ms (100%), Space: 10.6 MB (99.98%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n =digits.size();
+        digits[n-1]++;
+        for (int i = n-1; i>=0; i--){
+            if (digits[i]==10){
+                digits[i]=0;
+                if (i == 0){
+                    digits.insert(digits.begin(), 1);
+                }
+                else{
+                    digits[i-1]++;
+                }
+            }
+            else break;
+        }
+        return digits;
+    }
+};
