83c0425f73546260aa02fb561c3626e71a108ea7
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-08-01 15:45:00 +0700

    Time: 31 ms (75.58%), Space: 85.7 MB (83.56%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if (n<2) return 0;
+        sort(nums.begin(), nums.end());
+        int dis = 0;
+        for (int i = 0; i<n-1; i++){
+            dis = max(dis, nums[i+1]-nums[i]);
+        }
+        return dis;
+    }
+};
