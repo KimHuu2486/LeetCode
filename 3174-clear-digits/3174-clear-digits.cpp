9b1ff9e5ae5f05b312d6d3b5a601dd7f9c7252e7
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-08-01 15:54:03 +0700

    Time: 0 ms (100%), Space: 9.1 MB (36.49%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(!isdigit(s[i])){
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
+            }
+        }
+        string ans = "";
+        while (!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
