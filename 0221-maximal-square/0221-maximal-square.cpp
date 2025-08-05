9deb17f61aa13039c46dcd0fd88359545820ce66
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-08-05 16:44:29 +0700

    Time: 0 ms (100%), Space: 31.2 MB (15.23%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int histogramSquare(vector<int> vec) {
-        vec.push_back(0);
-        stack<int> s;
-        int square = 0;
-        for (int i = 0; i < vec.size(); i++) {
-            while (!s.empty() && vec[s.top()] > vec[i]) {
-                int height = vec[s.top()];
-                s.pop();
-                int width = s.empty() ? i : i - s.top() - 1;
-                int temp = pow(min(height, width), 2);
-                square = max(square, temp);
-            }
-            s.push(i);
-        }
-        return square;
-    }
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
         int maxSquare = 0;
-        vector<int> vec(n, 0);
+        vector<vector<int>> dp(m, vector<int>(n));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                vec[j] = (matrix[i][j] == '1') ? (vec[j] + 1) : 0;
+                dp[i][j] = matrix[i][j] - '0';
+                maxSquare = max(maxSquare, dp[i][j]);
             }
-            maxSquare = max(maxSquare, histogramSquare(vec));
         }
-        return maxSquare;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (dp[i][j] == 1) {
+                    dp[i][j] =
+                        min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) +
+                        1;
+                    maxSquare = max(maxSquare, dp[i][j]);
+                }
+            }
+        }
+        return maxSquare * maxSquare;
     }
 };
