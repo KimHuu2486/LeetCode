5ed5fd8d6887ddb00686704e809f7b6dc326392d
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-08-20 22:36:41 +0700

    Time: 1 ms (84.22%), Space: 27.6 MB (20.45%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,45 +1,34 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
+    unordered_map<int, int> idx; // giá trị -> vị trí trong inorder
+    
     TreeNode* solve(vector<int>& preorder, vector<int>& inorder,
                     pair<int, int> pre, pair<int, int> in) {
         if (pre.first > pre.second || in.first > in.second)
             return nullptr;
-        TreeNode* root = new TreeNode;
-        root->val = preorder[pre.first];
-        if (pre.first == pre.second) {
-            root->left = NULL;
-            root->right = NULL;
-            return root;
-        }
-        int i = in.first;
-        for (i; i <= in.second; i++) {
-            if (inorder[i] == root->val)
-                break;
-        }
-        int mid = i;
+
+        int rootVal = preorder[pre.first];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int mid = idx[rootVal]; // vị trí root trong inorder
         int leftSize = mid - in.first;
+
         pair<int, int> leftPre = {pre.first + 1, pre.first + leftSize};
         pair<int, int> rightPre = {pre.first + leftSize + 1, pre.second};
         pair<int, int> leftIn = {in.first, mid - 1};
         pair<int, int> rightIn = {mid + 1, in.second};
+
         root->left = solve(preorder, inorder, leftPre, leftIn);
         root->right = solve(preorder, inorder, rightPre, rightIn);
+
         return root;
     }
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = preorder.size();
+        for (int i = 0; i < n; i++) {
+            idx[inorder[i]] = i; // lưu vị trí trong inorder
+        }
         return solve(preorder, inorder, {0, n - 1}, {0, n - 1});
     }
 };
