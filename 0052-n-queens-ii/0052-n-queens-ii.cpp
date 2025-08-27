7b045d2e4f528ef7f8a1136bc4f78a276e0ca81e
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-08-27 22:10:05 +0700

    Time: 0 ms (100%), Space: 8 MB (82.54%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    void solve(int &count, vector<bool>col, vector<bool>d1, vector<bool>d2, vector<int>pos, int i, int n){
-        if (i == n){
+    void solve(int& count, vector<bool>& col, vector<bool>& d1,
+               vector<bool>& d2, int i, int n) {
+        if (i == n) {
             count++;
             return;
         }
-        for (int j = 0; j<n ;j++){
-            if (col[j] || d1[i-j+n-1] || d2[i+j]) continue;
-            pos[i] =j;
-            col[j] = 1;
-            d1[i-j+n-1] = 1;
-            d2[i+j] = 1;
-            solve(count, col, d1, d2, pos,i+1, n);
-            col[j] = 0;
-            d1[i-j+n-1] = 0;
-            d2[i+j] = 0;
+        for (int j = 0; j < n; j++) {
+            if (col[j] || d1[i - j + n - 1] || d2[i + j])
+                continue;
+            col[j] = true;
+            d1[i - j + n - 1] = true;
+            d2[i + j] = true;
+            solve(count, col, d1, d2, i + 1, n);
+            col[j] = false;
+            d1[i - j + n - 1] = false;
+            d2[i + j] = false;
         }
     }
     int totalNQueens(int n) {
         int count = 0;
-        vector<bool>col(n, 0);
-        vector<bool>d1(2*n-1, 0);
-        vector<bool>d2(2*n - 1, 0);
-        vector<int>pos(n, -1);
-        solve(count, col, d1, d2, pos, 0, n);
+        vector<bool> col(n, false);
+        vector<bool> d1(2 * n - 1, false);
+        vector<bool> d2(2 * n - 1, false);
+        solve(count, col, d1, d2, 0, n);
         return count;
     }
 };
