8dc7ae719afc9cc6ebe6e3a4272f963e6eaad22c
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-10-06 10:08:09 +0700

    Time: 3 ms (88.24%), Space: 13.1 MB (68.33%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,51 +1,33 @@
 class Solution {
-private:
-    const vector<pair<int, int>> direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    vector<vector<pair<int, int>>> adj;
-    vector<int> Distance;
-    vector<bool> visited;
-
 public:
-    void Dijkstra(int source) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            heap;
-        heap.push({Distance[source], source});
-        while (!heap.empty()) {
-            auto cur = heap.top();
-            heap.pop();
-            if (visited[cur.second])
-                continue;
-            visited[cur.second] = true;
-            for (auto v : adj[cur.second]) {
-                if (!visited[v.first] &&
-                    Distance[v.first] > max(Distance[cur.second], v.second)) {
-                    Distance[v.first] = max(Distance[cur.second], v.second);
-                    heap.push({Distance[v.first], v.first});
-                }
-            }
-        }
-    }
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        adj.resize(n * n, {});
-        visited.resize(n * n, false);
-        Distance.resize(n * n, INT_MAX);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int u = i * n + j;
-                for (int k = 0; k < 4; k++) {
-                    int newX = i + direction[k].first;
-                    int newY = j + direction[k].second;
-                    if (newX < 0 || newX >= n || newY < 0 || newY >= n)
-                        continue;
-                    int v = newX * n + newY;
-                    adj[u].push_back({v, grid[newX][newY]});
-                }
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            heap;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        heap.push({grid[0][0], {0, 0}});
+        ;
+        vector<int> dx = {1, -1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
+        while (!heap.empty()) {
+            auto it = heap.top();
+            heap.pop();
+            int height = it.first;
+            int curx = it.second.first;
+            int cury = it.second.second;
+            if (curx == n - 1 && cury == n - 1)
+                return height;
+            if (visited[curx][cury]) continue;
+            visited[curx][cury] = true;
+            for (int i = 0; i < 4; i++) {
+                int newx = curx + dx[i], newy = cury + dy[i];
+                if (newx < 0 || newx >= n || newy < 0 || newy >= n || visited[newx][newy])
+                    continue;
+                heap.push({max(height, grid[newx][newy]), {newx, newy}});
             }
         }
-        Distance[0] = grid[0][0];
-        Dijkstra(0);
-        return Distance[n * n - 1];
+        return 0;
     }
 };
