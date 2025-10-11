e71d8ed987f58318e06024e04e3d33b94f09ae2b
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-10-11 21:38:52 +0700

    Update 3186-maximum-total-damage-with-spell-casting.py

--- 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
@@ -1,13 +1,13 @@
 class Solution:
     def maximumTotalDamage(self, power: List[int]) -> int:
-        freq = Counter(power)
+        freq = Counter(power) #like unordered_map in C++
         value = sorted(freq.keys())
         n = len(value)
         dp = [0] * n
         dp[0] = value[0] * freq[value[0]]
         for i in range(n):
             take = value[i] * freq[value[i]]
-            prev = bisect.bisect_right(value, value[i] - 3) - 1
+            prev = bisect.bisect_right(value, value[i] - 3) - 1   #like upper_bound in C++
             if prev >= 0:
                 take += dp[prev]
             dp[i] = max(take, dp[i - 1])
