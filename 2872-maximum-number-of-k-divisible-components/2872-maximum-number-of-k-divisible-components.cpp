3584858871d0a10897fcfa50d6f89e4f76836bce
Tran Kim Huu <trankimhuu2006@gmail.com>
2025-11-28 15:53:38 +0700

    Time: 94 ms (86.83%), Space: 173.2 MB (93.24%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    void dfs(vector<int>&parent, vector<vector<int>>&child, vector<int>&values, int k, int u, int& cnt) {
-        if (child[u].size() != 0) {
-            for (int v: child[u]) {
-                dfs(parent, child, values, k, v, cnt);
+    vector<vector<int>>adj;
+    void dfs(vector<int>&values, int k, int parent, int u, int& cnt) {
+        for (int v: adj[u]) {
+            if (v != parent) {
+                dfs(values, k, u, v, cnt);
             }
         }
         if (values[u] % k == 0) {
             cnt++;
         }
         else {
-            values[parent[u]] = (values[parent[u]] + values[u]) % k;
+            values[parent] = (values[parent] + values[u]) % k;
         }
     }
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {    
-        vector<vector<int>>adj(n, vector<int>());
+        adj.resize(n, vector<int>());
         for (auto& e: edges) {
             int u = e[0];
             int v = e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<int>parent(n, INT_MIN);
-        vector<vector<int>>child(n, vector<int>());
-        parent[0] = 0;
-        queue<int> q;
-        q.push(0);
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            for (int v: adj[u]) {
-                if (parent[u] != v) {
-                    parent[v] = u;
-                    child[u].push_back(v);
-                    q.push(v);
-                }
-            }
-        }
         int cnt = 0;
-        dfs(parent, child, values, k, 0, cnt);
+        dfs(values, k, -1, 0, cnt);
         return cnt;
     }
 };
