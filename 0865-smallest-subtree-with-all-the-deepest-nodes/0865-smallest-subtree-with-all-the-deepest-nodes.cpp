1795ff47bf5d81dbf7a724ef8302c13eac9d1123
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-01-09 22:52:33 +0700

    Time: 0 ms (100%), Space: 16.3 MB (27.77%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -10,50 +10,24 @@
  * };
  */
 class Solution {
-private:
-    int maxLen = 0;
-    vector<vector<TreeNode*>>Paths;
 public:
-    void dfs(TreeNode* node, vector<TreeNode*>& path) {
-        path.push_back(node);
-        if (!node->left && !node->right) {
-            Paths.push_back(path);
-            maxLen = max(maxLen, (int)path.size());
-            path.pop_back();
-            return;
+    pair<int, TreeNode*> dfs(TreeNode* node) {
+        if (!node) {
+            return {0, nullptr};
         }
-        if (node->left) {
-            dfs(node->left, path);
+        auto L = dfs(node->left);
+        auto R = dfs(node->right);
+        if (L.first == R.first) {
+            return {L.first + 1, node};
         }
-        if (node->right) {
-            dfs(node->right, path);
+        else if (L.first > R.first) {
+            return {L.first + 1, L.second};
         }
-        path.pop_back();
+        else {
+            return {R.first + 1, R.second};
+        }
     }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        vector<TreeNode*>path;
-        dfs(root, path);
-        int start = 0;
-        int n = Paths.size();
-
-        for (int i = 0; i < n; i++) {
-            if (maxLen == Paths[i].size()) {
-                start = i;
-                break;
-            }
-        }
-
-        for (int j = 0; j < maxLen; j++) {
-            TreeNode* cur = Paths[start][j];
-            for (int i = start + 1; i < n; i++) {
-                if (maxLen == Paths[i].size()) {
-                    if (cur != Paths[i][j]) {
-                        return Paths[i][j - 1];
-                    }
-                }
-            }
-        }
-
-        return Paths[start][maxLen - 1];
+        return dfs(root).second;    
     }
 };
