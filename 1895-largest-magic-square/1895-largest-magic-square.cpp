2fa7c47ab326beb5a5c980163d36f5ef56c2c0fa
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-01-18 09:10:52 +0700

    Date: 01-18-2026 , Difficulty: Medium, Time: 7 ms (86.63%), Space: 16.8 MB (18.89%)

--- 1895-largest-magic-square/1895-largest-magic-square.cpp
+++ 1895-largest-magic-square/1895-largest-magic-square.cpp
@@ -1,69 +1,48 @@
 class Solution {
 public:
-    bool checkMagicSquare(const vector<vector<int>>& grid, int row, int col, int k) {
-        int target = 0;
-        for (int i = row; i < row + k; i++) {
-            int sumRow = 0;
-            for (int j = col; j < col + k; j++) {
-                sumRow += grid[i][j];
-            }
-            if (i == row) {
-                target = sumRow;
-            }
-            else {
-                if (target != sumRow) {
-                    return false;
-                }
-            }
+    bool isMagic(const vector<vector<vector<int>>>& prefixSum, int r, int c,
+                 int sz) {
+        int sum = prefixSum[r + sz][c + sz][2] - prefixSum[r][c][2];
+        if (sum != prefixSum[r + sz][c + 1][3] - prefixSum[r][c + sz + 1][3]) {
+            return false;
         }
 
-        for (int j = col; j < col + k; j++) {
-            int sumCol = 0;
-            for (int i = row; i < row + k; i++) {
-                sumCol += grid[i][j];
-            }
-            if (sumCol != target) {
+        for (int i = 0, j = r; i < sz; i++, j++) {
+            if (sum != prefixSum[j + 1][c + sz][0] - prefixSum[j + 1][c][0]) {
                 return false;
             }
         }
 
-        int sumDiagonal = 0;
-        int x = row, y = col;
-        while (x < row + k) {
-            sumDiagonal += grid[x][y];
-            x++;
-            y++;
-        }
-        if (sumDiagonal != target) {
-            return false;
-        }
-
-        int sumAntiDiagonal = 0;
-        x = row;
-        y = col + k - 1;
-        while (x < row + k) {
-            sumAntiDiagonal += grid[x][y];
-            x++;
-            y--;
-        }
-        if (sumAntiDiagonal != target) {
-            return false;
+        for (int i = 0, j = c; i < sz; i++, j++) {
+            if (sum != prefixSum[r + sz][j + 1][1] - prefixSum[r][j + 1][1])
+                return false;
         }
         return true;
     }
+
     int largestMagicSquare(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int k = min(m, n);
-        while (k > 1) {
-            for (int i = 0; i <= m - k; i++) {
-                for (int j = 0; j <= n - k; j++) {
-                    if (checkMagicSquare(grid, i, j, k)) {
+        int m = grid.size(), n = grid[0].size();
+        // {0,1,2,3} = {row,col,diag,anti-diag}
+        vector<vector<vector<int>>> prefixSum(
+            m + 1, vector<vector<int>>(n + 2, vector<int>(4, 0)));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                int val = grid[i - 1][j - 1];
+                prefixSum[i][j][0] = prefixSum[i][j - 1][0] + val;
+                prefixSum[i][j][1] = prefixSum[i - 1][j][1] + val;
+                prefixSum[i][j][2] = prefixSum[i - 1][j - 1][2] + val;
+                prefixSum[i][j][3] = prefixSum[i - 1][j + 1][3] + val;
+            }
+        }
+
+        int sz = min(m, n);
+        for (int k = sz; k >= 2; k--) {
+            for (int i = 0; i + k <= m; i++) {
+                for (int j = 0; j + k <= n; j++) {
+                    if (isMagic(prefixSum, i, j, k))
                         return k;
-                    }
                 }
             }
-            k--;
         }
         return 1;
     }
