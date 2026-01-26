70078ddb773b7433845a2e17aaeec7ae1890ed81
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-01-26 08:58:21 +0700

    Date: 01-26-2026 , Difficulty: Easy, Time: 15 ms (76.77%), Space: 36.8 MB (32.02%)

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -4,13 +4,14 @@ public:
         int n = arr.size();
         sort(arr.begin(), arr.end());
         int minDiff = INT_MAX;
-        for (int i = 0; i < n - 1; i++) {
-            minDiff = min(minDiff, arr[i + 1] - arr[i]);
-        }
-
         vector<vector<int>> ans;
         for (int i = 0; i < n - 1; i++) {
-            if (arr[i + 1] - arr[i] == minDiff) {
+            int curDiff = arr[i + 1] - arr[i];
+            if (curDiff <= minDiff) {
+                if (curDiff < minDiff) {
+                    ans.clear();
+                    minDiff = curDiff;
+                }
                 ans.push_back({arr[i], arr[i + 1]});
             }
         }
