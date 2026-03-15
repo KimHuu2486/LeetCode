d2adb731dfda5dea9dffff4aba9b0a86b246b9d7
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-03-15 15:05:47 +0700

    Date: 03-15-2026 , Difficulty: Easy, Time: 0 ms (100%), Space: 28.2 MB (10%)

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        vector<int>freq(101, 0);
+        for (int num: nums) {
+            freq[num]++;
+        }
+        for (int num: nums) {
+            if (num % 2 == 0) {
+                if (freq[num] == 1) {
+                    return num;
+                }
+            }
+        }
+        return -1;
+    }
+};
