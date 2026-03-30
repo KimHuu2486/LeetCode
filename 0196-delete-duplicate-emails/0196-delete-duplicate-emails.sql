d497287bab4a71084171505ac3165ea00b367a51
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-03-30 23:23:28 +0700

    Date: 03-30-2026 , Difficulty: Easy, Time: 1140 ms (7.11%), Space: 0B (100%)

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,10 +1,4 @@
 # Write your MySQL query statement below
-DELETE FROM Person
-WHERE id not in (
-    SELECT id
-    FROM (
-        SELECT MIN(id) as id
-        FROM Person
-        GROUP BY email
-    ) temp
-)
+DELETE p1
+FROM Person p1
+JOIN Person p2 ON p1.email = p2. email AND p1.id > p2.id
