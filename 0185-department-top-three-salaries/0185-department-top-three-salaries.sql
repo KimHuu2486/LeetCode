639e51c2df5f872a11ac22550d3449041129df3a
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-04-16 10:44:00 +0700

    Date: 04-16-2026 , Difficulty: Hard, Time: 976 ms (43.79%), Space: 0B (100%)

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select Department, Employee, Salary
+from (
+    select d.name as Department,
+            e.name as Employee,
+            e.salary as Salary,
+            DENSE_RANK() OVER (
+                PARTITION BY e.departmentId
+                ORDER BY e.salary DESC
+            ) as rk
+    from Employee e
+    join Department d on e.departmentId = d.id
+) as temp
+where rk <= 3
