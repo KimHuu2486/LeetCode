e82f240d6f7f11f77674f333eaffc85649784dbe
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-04-16 10:01:56 +0700

    Date: 04-16-2026 , Difficulty: Medium, Time: 483 ms (18.87%), Space: 0B (100%)

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    DECLARE M INT;
+    SET M = N - 1;
+    RETURN (
+      select (
+        select distinct salary
+        from Employee
+        order by salary desc
+        limit 1 offset M
+    )
+
+  );
+END
