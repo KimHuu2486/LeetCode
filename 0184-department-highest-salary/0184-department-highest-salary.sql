c77e89e170027b932f61f1d22743c791b0855d5f
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-04-16 10:29:30 +0700

    Date: 04-16-2026 , Difficulty: Medium, Time: 1047 ms (15.15%), Space: 0B (100%)

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee, e.Salary
+from Employee e
+join Department d on e.departmentId = d.id
+where e.Salary = (
+    select max(salary)
+    from Employee 
+    where departmentId = e.departmentId
+)
