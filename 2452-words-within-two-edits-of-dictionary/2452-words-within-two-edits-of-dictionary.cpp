a128c2af07da06f782268b00e0296251d9dac5b7
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-04-22 22:11:20 +0700

    Date: 04-22-2026 , Difficulty: Medium, Time: 0 ms (100%), Space: 12.5 MB (70.59%)

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n = queries.size(), m = dictionary.size(), len = queries[0].size();
+        vector<string>ans;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int cnt = 0;
+                for (int k = 0; k < len; k++) {
+                    if (queries[i][k] != dictionary[j][k]) {
+                        cnt++;
+                        if (cnt > 2) break;
+                    }
+                }
+
+                if (cnt <= 2) {
+                    ans.push_back(queries[i]);
+                    break;
+                };
+            } 
+        }
+
+        return ans;
+    }
+};
