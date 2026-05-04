30cf59d7338fba117a11572f5040da119b8c820c
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-05-04 11:07:43 +0700

    Date: 05-04-2026 , Difficulty: Medium, Time: 0 ms (100%), Space: 19.3 MB (28.55%)

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrix[:] = [list(row) for row in zip(*matrix[::-1])]
+        
