fd3d82191f4480ad15eae0bdf0cb088cd89ebe42
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-05-04 16:54:50 +0700

    Date: 05-04-2026 , Difficulty: Medium, Time: 315 ms (43.16%), Space: 0B (100%)

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+with temp as (
+    select requester_id id
+    from RequestAccepted
+    union all
+    select accepter_id id
+    from RequestAccepted
+)
+select id, count(*) as num
+from temp
+group by id
+order by num desc
+limit 1
