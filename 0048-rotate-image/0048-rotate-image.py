7e362b211ca401890e9ed53c69b18b7beaaf0742
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-05-04 11:10:59 +0700

    Date: 05-04-2026 , Difficulty: Medium, Time: 0 ms (100%), Space: 19.2 MB (92.56%)

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,5 +3,10 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix[:] = [list(row) for row in zip(*matrix[::-1])]
-        
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            matrix[i].reverse()        
