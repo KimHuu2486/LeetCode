77b49d957a297f250767eb6cdbf606670ebe3627
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-05-12 21:36:31 +0700

    Date: 05-12-2026 , Difficulty: Medium, Time: 1708 ms (6.83%), Space: 0B (100%)

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select user_id as buyer_id, join_date, count(order_id) as orders_in_2019
+from Users
+left join Orders on buyer_id = user_id and year(order_date) = 2019
+group by user_id, join_date
