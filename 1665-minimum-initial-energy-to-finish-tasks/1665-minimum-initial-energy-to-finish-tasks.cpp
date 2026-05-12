1fe004817611667804b4d8c9a097f4c22da58fc9
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-05-12 21:25:10 +0700

    Date: 05-12-2026 , Difficulty: Hard, Time: 23 ms (99.03%), Space: 97.3 MB (63.11%)

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -6,7 +6,7 @@ public:
              [](auto& a, auto& b) { return a[1] - a[0] < b[1] - b[0]; });
 
         int ans = 0;
-        for (auto task : tasks) {
+        for (auto& task : tasks) {
             ans = max(ans + task[0], task[1]);
         }
 
