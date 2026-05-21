0933cf756a2a8dbf663fde0b40e7d60448f289e7
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-05-21 11:07:09 +0700

    Date: 05-21-2026 , Difficulty: Medium, Time: 1416 ms (28.44%), Space: 0B (100%)

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+(
+    select u.name as results
+    from Users u
+    join MovieRating mr on u.user_id = mr.user_id
+    group by u.user_id
+    order by count(u.user_id) desc, u.name asc
+    limit 1
+)
+union all
+(
+    select m.title as results
+    from Movies m
+    join  MovieRating mr on m.movie_id = mr.movie_id
+    where created_at between "2020-02-01" and "2020-02-29"
+    group by m.movie_id
+    order by avg(rating) desc, m.title asc
+    limit 1
+)
