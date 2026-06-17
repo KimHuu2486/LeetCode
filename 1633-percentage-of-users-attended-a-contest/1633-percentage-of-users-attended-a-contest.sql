7e7153da5c2768e05e19fcab94a77f108e6b1864
Trần Kim Hữu <trankimhuu2006@gmail.com>
2026-06-17 09:59:09 +0700

    Date: 06-17-2026 , Difficulty: Easy, Time: 1026 ms (40.61%), Space: 0B (100%)

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select contest_id, round(1.0 * count(*) / (
+    select count(*)
+    from Users
+) * 100.0, 2) as percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id asc 
